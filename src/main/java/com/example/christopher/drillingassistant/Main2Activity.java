436945647b17bdc9a31f7e45a4c646914eb97c54
christopher-maier <christopher.maier@students.htlinn.ac.at>
2018-09-22 22:01:24 +0200

    Add files via upload

--- src/main/java/com/example/christopher/drillingassistant/Main2Activity.java
+++ src/main/java/com/example/christopher/drillingassistant/Main2Activity.java
@@ -8,8 +8,15 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+
+    double length;
+    double width;
+    double holes;
+    double distance;
+    double diameter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +24,74 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Intent intent = getIntent();
-        int length = intent.getIntExtra(MainActivity.EXTRA_NUMBER0, 0);
+        length = intent.getIntExtra(MainActivity.EXTRA_NUMBER0, 0);
         TextView text_length = (TextView) findViewById(R.id.text_length);
-        text_length.setText("length of your board: " + length);
+        text_length.setText("length of your board: " + (int)length);
 
-        int width = intent.getIntExtra(MainActivity.EXTRA_NUMBER1, 0);
+        width = intent.getIntExtra(MainActivity.EXTRA_NUMBER1, 0);
         TextView text_width = (TextView) findViewById(R.id.text_width);
-        text_width.setText("width of your board: " + width);
+        text_width.setText("width of your board: " + (int)width);
 
-        int holes = intent.getIntExtra(MainActivity.EXTRA_NUMBER2, 0);
+        holes = intent.getIntExtra(MainActivity.EXTRA_NUMBER2, 0);
         TextView text_holes = (TextView) findViewById(R.id.text_holes);
-        text_holes.setText("max number of holes: " + holes);
+        text_holes.setText("max number of holes: " + (int)holes);
 
-        int distance = intent.getIntExtra(MainActivity.EXTRA_NUMBER3, 0);
+        distance = intent.getIntExtra(MainActivity.EXTRA_NUMBER3, 0);
         TextView text_distance = (TextView) findViewById(R.id.text_distance);
-        text_distance.setText("max distance between holes: " + distance);
+        text_distance.setText("max distance between holes: " + (int)distance);
 
-        int diameter = intent.getIntExtra(MainActivity.EXTRA_NUMBER4, 0);
+        diameter = intent.getIntExtra(MainActivity.EXTRA_NUMBER4, 0);
             TextView text_diameter = (TextView) findViewById(R.id.text_diameter);
-            text_diameter.setText("max diameter: " + diameter);
+            text_diameter.setText("max diameter: " + (int)diameter);
+
+        double endresult_holes = maxHoles(length, width, distance, diameter);
+        TextView result_holes = findViewById(R.id.result_holes);
+        result_holes.setText("" + (int) endresult_holes);
+
+        /*double endresult_distance = maxDistance(length,width,distance,holes);
+        TextView result_distance = findViewById(R.id.result_distance);
+        result_distance.setText("" + endresult_distance);
+
+        double endresult_diameter = maxDiameter(length,width,distance, holes);
+        TextView result_diameter = findViewById(R.id.result_diameter);
+        result_diameter.setText("" + (int) endresult_diameter);*/
     }
+
+    public double maxHoles(double length, double width, double distance, double diameter) {
+        double i;
+        if(length < diameter || width < diameter) {
+            System.out.println("Invalid");
+            i = 0;
+        }else {
+            i = ((length - (2 * distance))/ (diameter + distance));
+        }
+        /*double VK = (int) i;
+        double NK = i - VK;
+        if(NK != 0) {
+            Toast.makeText(this, NK + "mm müssen gleichmäßig auf die Abstände verteilt werden!", Toast.LENGTH_SHORT).show();
+        }*/
+        return i;
+    }
+
+  /*  public double maxDistance(double length, double width, double diameter, double holes) {
+        double j;
+        if(length < (holes * diameter) || width < diameter) {
+            System.out.println("Invalid");
+            j = 0;
+        }else {
+            j = ((length - (holes * diameter)) / (holes +2));
+        }
+        return j;
+    }
+
+    public double maxDiameter(double length, double width, double distance, double holes) {
+        double i;
+        if(length < distance || width < distance) {
+            System.out.println("Invalid");
+            i = 0;
+        }else {
+            i = ((distance * (holes + 2) - length)/ (-holes));
+        }
+        return i;
+    }*/
 }
